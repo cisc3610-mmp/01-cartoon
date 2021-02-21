10c55de8f3d272f0cdd7953e200787a7d37ab382
Ricky Chon <rickychon99@gmail.com>
2021-02-21 09:12:00 -0500

    Refactor to use canvas variable

--- scripts/cartoon.js
+++ scripts/cartoon.js
@@ -5,15 +5,16 @@ function init()
 
 function draw()
 {
-    var ctx = document.getElementById('canvas').getContext('2d');
+    const canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
 
     // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation
     ctx.globalCompositeOperation = 'source-over';
-    ctx.clearRect(0, 0, 800, 800); // clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
 
     // Sky
     ctx.fillStyle = 'rgb(135, 206, 235)';
-    ctx.fillRect(0, 0, 800, 800);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.save();
 
     window.requestAnimationFrame(draw);
